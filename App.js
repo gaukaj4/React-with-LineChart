513407ddb07d779488ed1775ea7b0fa1da0db78b
gaukaj4 <49602762+gaukaj4@users.noreply.github.com>
2021-01-26 12:14:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LineChart from './components/LineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='chart'>
+        <LineChart />        
+        </div>
+    </div>
+  );
+}
+
+export default App;
